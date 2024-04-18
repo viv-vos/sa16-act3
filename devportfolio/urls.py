7b93128e4f6d2c0ab25c1c97bdc03c14f05f0c88
Vivian Ly <viv.ly.2803@gmail.com>
2024-04-17 20:17:31 -0500

    Final Solutions

--- devportfolio/urls.py
+++ devportfolio/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+from pages import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home),
+
+    path("", RedirectView.as_view(url="home/", permanent=True)),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('work/', views.work),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def work(request):
+    return render(request, 'pages/work.html')
